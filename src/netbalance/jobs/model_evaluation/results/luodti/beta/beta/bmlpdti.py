4c88062200a68eecba0029ffb217b0f2ccbb18c4
sobhanAhmadian <sobhan.ahmadian.moghadam@gmail.com>
2025-02-01 20:49:32 +0330

    chore(jobs): update parameters

--- src/netbalance/jobs/model_evaluation/results/luodti/beta/beta/bmlpdti.py
+++ src/netbalance/jobs/model_evaluation/results/luodti/beta/beta/bmlpdti.py
@@ -17,8 +17,8 @@ test_balance_method = "beta"  # Parameter
 test_balance_kwargs = {}  # Parameter
 test_balance_negative_ratio = 1.0  # Parameter
 
-num_cross_validation = 1  # Parameter
-num_negative_sampling = 1  # Parameter
+num_cross_validation = 5  # Parameter
+num_negative_sampling = 5  # Parameter
 
 model_result_dir = os.path.join(
     RESULTS_DIR,
