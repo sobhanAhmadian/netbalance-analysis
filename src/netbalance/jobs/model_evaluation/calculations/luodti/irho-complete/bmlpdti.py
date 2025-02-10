3e261c24449adb58d608aa658cab941cf23a4792
sobhanAhmadian <sobhan.ahmadian.moghadam@gmail.com>
2025-02-10 20:50:56 +0330

    feat(jobs): irho-complete initial version

---
+++ src/netbalance/jobs/model_evaluation/calculations/luodti/irho-complete/bmlpdti.py
@@ -0,0 +1,104 @@
+import os
+
+import numpy as np
+
+from netbalance.configs.bmlpdti import BMLPDTI_RESULTS_DIR as RESULTS_DIR  # Parameter
+from netbalance.configs.bmlpdti import BMLPDTIModelConfig as ModelConfig  # Parameter
+from netbalance.configs.bmlpdti import (
+    BMLPDTIOptimizerConfig as OptimizerConfig,
+)  # Parameter
+from netbalance.data.bipartite_graph_data import BGData, BGTrainTestSpliter
+from netbalance.evaluation import repeated_cross_validation
+from netbalance.features.luodti import LuoDTIDataset as Dataset  # Parameter
+from netbalance.models.bmlpdti import (
+    BMLPDTIHandlerFactory as HandlerFactory,
+)  # Parameter
+from netbalance.optimization.bmlpdti import (
+    BalanceBMLPDTITrainer as Trainer,
+)  # Parameter
+from netbalance.utils import prj_logger
+
+logger = prj_logger.getLogger(__name__)
+
+model_name = "bmlpdti"  # Parameter
+dataset = "luodti"  # Parameter
+train_neg_samp_method = "ibeta_complete"  # Parameter
+
+num_cross_validation = 1  # Parameter
+
+splitter_kwargs = {
+    "k": 5,
+    "train_balance": False,  # Parameter
+    "train_balance_kwargs": {},  # Parameter
+}
+
+model_result_dir = os.path.join(
+    RESULTS_DIR,
+    f"preds",
+    f"dataset_{dataset}",
+    f"train_neg_samp_{train_neg_samp_method}",
+)
+
+os.makedirs(model_result_dir, exist_ok=True)
+
+logger.info(
+    f">>>>>>>>>>>>>>>>> Job: Model Evaluation - {dataset} - {model_name} - {train_neg_samp_method}"
+)
+
+ds = Dataset()
+
+model_config = ModelConfig()
+model_config.drug_num = len(ds.get_cluster_a_node_names())
+model_config.protein_num = len(ds.get_cluster_b_node_names())
+model_config.input_dim = len(ds.get_cluster_a_node_names()) + len(
+    ds.get_cluster_b_node_names()
+)
+model_config.hidden_dim = 64
+
+optimizer_config = OptimizerConfig()  # Parameter
+optimizer_config.i_balance_method = "beta"
+optimizer_config.i_balance_kwargs = {
+    "max_iter": 40000,
+    "delta": 0.1,
+    "cooling_rate": 0.99,
+    "initial_temp": 40.0,
+    "ent_desired": 1.0,
+    "shrinkage": 1.0,
+}
+optimizer_config.i_negative_ratio = 1.0
+optimizer_config.fair = True
+optimizer_config.n_epoch = 10
+optimizer_config.lr = 0.001
+
+associations = ds.get_associations(with_negatives=True)
+
+trainer = Trainer()
+factory = HandlerFactory(model_config=model_config)  # Parameter
+model_handler = factory.create_handler()
+data = BGData(
+    associations=ds.get_associations(with_negatives=True),
+    cluster_a_node_names=ds.get_cluster_a_node_names(),
+    cluster_b_node_names=ds.get_cluster_b_node_names(),
+)
+
+# Train the model
+trainer.train(model_handler=model_handler, data=data, config=optimizer_config)
+
+# Generate Predictions
+model_handler.model.eval()
+batch_size = 1000
+preds = np.zeros(data.associations.shape[0])
+for j in range(0, data.associations.shape[0], batch_size):
+    preds[j : j + batch_size] = model_handler.predict(
+        a_nodes=data.associations[j : j + batch_size, 0],
+        b_nodes=data.associations[j : j + batch_size, 1],
+    )
+logger.info("Predictions generated.")
+file = f"{model_result_dir}/preds.csv"
+with open(file, "w") as f:
+    f.write("Node A,Node B,Association,Score\n")
+    for i in range(len(data.associations)):
+        f.write(
+            f"{data.associations[i, 0]},{data.associations[i, 1]},{data.associations[i, 2]},{preds[i]}\n"
+        )
+    print(f"Predictions saved to {file}")
---
+++ src/netbalance/jobs/model_evaluation/results/luodti/irho-complete/bmlpdti.py
@@ -0,0 +1,44 @@
+import os
+
+import pandas as pd
+
+from netbalance.configs.bmlpdti import BMLPDTI_RESULTS_DIR as RESULTS_DIR  # Parameter
+from netbalance.configs.common import RESULTS_DIR as COMMON_RESULTS_DIR
+from netbalance.features.luodti import LuoDTIDataset as Dataset  # Parameter
+from netbalance.utils import prj_logger
+
+logger = prj_logger.getLogger(__name__)
+
+model_name = "bmlpdti"  # Parameter
+dataset = "luodti"  # Parameter
+train_neg_samp_method = "ibeta_complete"  # Parameter
+
+analyse = "watch hit at k"  # Parameter
+
+logger.info(
+    f">>>>>>>>>>>>>>>>> Job: Model Evaluation - Results - {dataset} - {model_name} - {train_neg_samp_method} - {analyse}"
+)
+
+model_result_dir = os.path.join(
+    RESULTS_DIR,
+    f"preds",
+    f"dataset_{dataset}",
+    f"train_neg_samp_{train_neg_samp_method}",
+)
+
+ds = Dataset()
+
+preds_file = os.path.join(model_result_dir, f"preds.csv")
+df = pd.read_csv(preds_file)
+
+cluster_a_node_names = ds.get_cluster_a_node_names()
+cluster_b_node_names = ds.get_cluster_b_node_names()
+a_node_names = [cluster_a_node_names[i] for i in df.iloc[:, 0]]
+b_node_names = [cluster_b_node_names[i] for i in df.iloc[:, 1]]
+df['Drug'] = a_node_names
+df["Target"] = b_node_names
+
+zero_df = df.loc[df.iloc[:, 2] == 0]
+zero_df = zero_df.sort_values(by=["Score"], ascending=False)
+
+print(zero_df.head(10))
