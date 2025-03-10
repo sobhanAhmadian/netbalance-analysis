8ecf7d9f273cbc08df7b6f2b2ba7094a75e18d2e
Sobhan Ahmadian <sobhan.ahmadian.moghadam@gmail.com>
2025-03-10 13:10:40 +0330

    feat: irho for bmlpsyn

--- src/netbalance/optimization/bmlpsyn.py
+++ src/netbalance/optimization/bmlpsyn.py
@@ -1,9 +1,11 @@
 import copy
 import hashlib
-from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
 import torch
+
+import dask
+from dask.distributed import Client, LocalCluster
 
 from netbalance.configs.bmlpsyn import BMLPSYNOptimizerConfig
 from netbalance.data import PytorchData
@@ -13,7 +15,8 @@ from netbalance.evaluation.utils import evaluate_binary_classification_simple
 from netbalance.models.bmlpsyn import BMLPSYNModelHandler
 from netbalance.optimization.simple_pytorch import PytorchTrainer
 from netbalance.utils import get_header_format, prj_logger
-
+import os
+from tqdm import tqdm
 from .interface import Trainer
 
 logger = prj_logger.getLogger(__name__)
@@ -72,11 +75,7 @@ class BalanceBMLPSYNTrainer(Trainer):
         config: BMLPSYNOptimizerConfig,
     ) -> Result:
         logger.info(get_header_format("Training the model"))
-
-        if config.fair:
-            model_handler.fe.build(data.associations)
-        else:
-            model_handler.fe.build()
+        model_handler.fe.build()
 
         ###########################################################
         hash_associations = hash_numpy_array(data.associations)
@@ -87,7 +86,7 @@ class BalanceBMLPSYNTrainer(Trainer):
 
         def task(num_bal):
             save_name = (
-                f"i{config.i_balance_method}_bmlpsyn/{hash_associations}_{num_bal}"
+                f"i{config.i_balance_method}_bmlpsyn_{hash_associations}_{num_bal}"
             )
             logger.info(
                 f"Parallel balancing data with {config.i_balance_method} in epoch {num_bal}"
@@ -103,13 +102,28 @@ class BalanceBMLPSYNTrainer(Trainer):
             logger.info(f"balanced data with {config.i_balance_method} in epoch {e}")
 
         # Multi thread run tasks for num_bal in range 1 to 20
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            executor.map(task, range(0, 20))
+        tasks = [dask.delayed(task)(num_bal) for num_bal in range(0, config.i_max_num_bal)]
 
+        with (
+            tqdm(
+                total=config.i_max_num_bal,
+                desc="Sub-training datasets",
+            ) as pbar,
+            Client(
+                LocalCluster(
+                    n_workers=int(os.getenv("NUM_WORKERS")),
+                    threads_per_worker=int(os.getenv("THREADS_PER_WORKER")),
+                )
+            ) as client,
+        ):
+            futures = client.compute(tasks)
+            for future in dask.distributed.as_completed(futures):
+                pbar.update(1)
+        
         for e in range(config.n_epoch):
-            num_bal = e % 20
+            num_bal = e % config.i_max_num_bal
             save_name = (
-                f"i{config.i_balance_method}_bmlpsyn/{hash_associations}_{num_bal}"
+                f"i{config.i_balance_method}_bmlpsyn_{hash_associations}_{num_bal}"
             )
 
             e_data = copy.deepcopy(data)
@@ -124,12 +138,13 @@ class BalanceBMLPSYNTrainer(Trainer):
 
             associations = e_data.associations
             dp_embed = model_handler.fe.extract_features(
-                associations[:, 0], associations[:, 1]
-            ).numpy()
-            y = np.array(associations[:, 2].tolist(), dtype=np.float32).reshape(-1, 1)
+                associations[:, 0], associations[:, 1], associations[:, 2]
+            )
+            y = np.array(associations[:, -1].tolist(), dtype=np.float32).reshape(-1, 1)
+            
             simple_data = PytorchData(
-                X=torch.tensor(dp_embed).to(config.device),
-                y=torch.tensor(y).to(config.device),
+                X=torch.tensor(dp_embed, dtype=torch.float32).to(device),
+                y=torch.tensor(y, dtype=torch.float32).to(device),
             )
 
             pytorch_trainer = PytorchTrainer()
@@ -137,14 +152,10 @@ class BalanceBMLPSYNTrainer(Trainer):
 
         ###########################################################
 
-        test_batch_size = 1000
-        preds = np.zeros(data.associations.shape[0])
-        for j in range(0, data.associations.shape[0], test_batch_size):
-            preds[j : j + test_batch_size] = model_handler.predict(
-                a_nodes=data.associations[j : j + test_batch_size, 0],
-                b_nodes=data.associations[j : j + test_batch_size, 1],
-            )
+        preds = model_handler.predict(
+            [data.associations[:, i] for i in range(data.associations.shape[1] - 1)]
+        )
         result = evaluate_binary_classification_simple(
-            data.associations[:, 2], preds.reshape(-1), config.threshold
+            data.associations[:, -1], preds.reshape(-1), config.threshold
         )
         return result
