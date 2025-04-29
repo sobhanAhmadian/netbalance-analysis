53e45c9b725422822f001b8c7c09613a18b37a86
sobhanAhmadian <sobhan.ahmadian.moghadam@gmail.com>
2025-04-29 19:22:18 +0330

    feat: sanger degree ratio models

---
+++ src/netbalance/configs/c_degree_ratio.py
@@ -0,0 +1,6 @@
+import os
+
+from .common import PROCESSED_DATA_DIR
+
+C_DEGREE_RATIO_PROCESSED_DATA_DIR = os.path.join(PROCESSED_DATA_DIR, "c_degree_ratio")
+C_DEGREE_RATIO_RESULTS_DIR = os.path.join(C_DEGREE_RATIO_PROCESSED_DATA_DIR, "results")
