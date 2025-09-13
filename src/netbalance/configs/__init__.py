ba89ea7f0b98ae7ad07ddb3c8e4f75321ff40011
sobhanAhmadian <sobhan.ahmadian.moghadam@gmail.com>
2025-09-13 12:20:43 +0200

    clean up repo

--- src/netbalance/configs/__init__.py
+++ src/netbalance/configs/__init__.py
@@ -2,11 +2,9 @@ from .a_degree_ratio import A_DEGREE_RATIO_RESULTS_DIR
 from .b_degree_ratio import B_DEGREE_RATIO_RESULTS_DIR
 from .common import LOG_DIR
 from .general import MethodConfig, ModelConfig, OptimizerConfig
-from .nvmda import NVMDA_RESULTS_DIR
 from .weighted_mean_degree_ratio import WEIGHTED_MEAN_DEGREE_RATIO_RESULTS_DIR
 
 RESULTS_DIR_DICT = {
-    "nvmda": NVMDA_RESULTS_DIR,
     "a_degree_ratio": A_DEGREE_RATIO_RESULTS_DIR,
     "b_degree_ratio": B_DEGREE_RATIO_RESULTS_DIR,
     "weighted_mean_degree_ratio": WEIGHTED_MEAN_DEGREE_RATIO_RESULTS_DIR,
--- src/netbalance/features/__init__.py
+++ src/netbalance/features/__init__.py
@@ -1,4 +1,2 @@
-from .disbiome import DisbiomDataset
-from .hmdad import HMDADDataset
 from .luodti import LuoDTIDataset
 from .sanger import SangerDataset
