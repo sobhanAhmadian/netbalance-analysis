e9386666460e60aa49c480609bb68751b3dc44c8
Ahmadian Moghadam Sobhan <ahmadian@grpaoli-haas066.intranet.epfl.ch>
2026-05-01 13:32:47 +0200

    chore: run results tasks for bmlpphi - irho training model

--- src/netbalance/jobs/model_evaluation/results/ecoli/irho/rho/bmlpphi.py
+++ src/netbalance/jobs/model_evaluation/results/ecoli/irho/rho/bmlpphi.py
@@ -23,7 +23,7 @@ test_balance_kwargs = {
 }  # Parameter
 test_balance_negative_ratio = 1.0  # Parameter
 
-num_cross_validation = 3  # Parameter
+num_cross_validation = 5  # Parameter
 num_negative_sampling = 5  # Parameter
 
 model_result_dir = os.path.join(
